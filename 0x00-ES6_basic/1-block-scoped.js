5a2192aa74c76d001f9eaf3f32cb883d364ddebe
Daniel Berihun <danielshitaye10@gmail.com>
2024-10-03 10:50:42 +0000

    var and hoisting

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  var task;
+  var task2;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    var task = true;
+    var task2 = false;
   }
+	task = false;
+	task2 = true;
 
   return [task, task2];
 }
