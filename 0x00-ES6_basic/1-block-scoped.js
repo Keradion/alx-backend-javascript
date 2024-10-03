61008cba02169e9a45c6a015ba2a3da6205a000b
Daniel Berihun <danielshitaye10@gmail.com>
2024-10-03 11:28:24 +0000

    var

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    task = true;
+    task2 = false;
   }
+  var task = false;
+  var task2 = true;
 
   return [task, task2];
 }
