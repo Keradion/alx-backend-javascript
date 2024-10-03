88de8f1839a1ac594178ab6c1ff92f00d630ae0d
Daniel Berihun <danielshitaye10@gmail.com>
2024-10-03 11:38:33 +0000

    replace var with let

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
 export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true; /* eslint-disable-line no-unused-vars */
+    const task2 = false; /* eslint-disable-line no-unused-vars */
   }
-  var task = false;
-  var task2 = true;
 
   return [task, task2];
 }
