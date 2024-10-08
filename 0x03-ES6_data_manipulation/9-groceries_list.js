38defe2d92af7154511f45f663914857ca47878a
Daniel Berihun <danielshitaye10@gmail.com>
2024-10-08 21:45:55 +0000

    map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+export default function groceriesList() {
+  // Creating a new Map
+  const list = new Map();
+
+  // Adding keys and values
+  list.set('Apples', 10);
+  list.set('Tomatoes', 10);
+  list.set('Pasta', 1);
+  list.set('Rice', 1);
+  list.set('Banana', 5);
+
+  return list;
+}
