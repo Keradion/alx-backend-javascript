4c2b05f19aecbae07b95e475d9ba4426096228b7
Daniel Berihun <danielshitaye10@gmail.com>
2024-10-08 22:01:20 +0000

    update map key value

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,11 @@
 export default function updateUniqueItems(map) {
   for (const [key, value] of map) {
     if (value === 1) {
-      map.set(key, 100);
+      try {
+	      map.set(key, 100);
+      } catch(err) {
+	      console.error('Cannot process');
+      }
     }
   }
 }
