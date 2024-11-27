70a5802c21e15f869fc5bd004cb4165957e0d75d
Daniel Berihun <danielshitaye10@gmail.com>
2024-11-27 08:43:22 +0000

    correct output

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,5 @@
 const displayMessage = () => {
-	process.stdout.write('Hello NodeJS!\n')
+	console.log('Hello NodeJS!\n')
 };
 // displayMessage can be import from other files
 module.exports = displayMessage;
