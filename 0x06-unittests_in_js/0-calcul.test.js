495c3ac53214745314232e2f178d2bd0466e7c37
Daniel Berihun <danielshitaye10@gmail.com>
2024-11-28 13:35:18 +0000

    more test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,4 +6,15 @@ describe('Testing calculateNumber function', () => {
 		result = calculateNumber(1.5, 2.5); // The expected sum result is 5 here.
 		assert.equal(result, 5);
 	});
+
+	it('Test if the function rounds and return correct sum of 2 digits', () => {
+		result = calculateNumber(1, 2.3); // The expected sum result is 3 here.
+		assert.equal(result, 3);
+	});
+	
+	it('Test if the function rounds and return correct sum of 2 digits', () => {
+		result = calculateNumber(2.3, 2.3); // The expected sum result is 4 here.
+		assert.equal(result, 4);
+	});
 });
+
