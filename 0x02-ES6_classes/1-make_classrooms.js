28ef6be0b55b36fef86f2dbc9881831c265c9352
Daniel <danielshitaye10@gmail.com>
2025-03-17 13:23:30 +0300

    lint issues fixed

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,17 +1,18 @@
-// we are importing class ClassRoom 
-import ClassRoom from './0-classroom.js'
+import ClassRoom from './0-classroom';
 
-export default function initalizeRooms (){
+export default function initalizeRooms() {
   // Initalize an empty array to collect ClassRoom Objects here
-  const classRoomObjects = []
+  const classRoomObjects = [];
 
   // Create 3 ClassRoom Objects 
-  const object1 = new ClassRoom(19)
-  const object2 = new ClassRoom(20)
-  const object3 = new ClassRoom(34)
+  const object1 = new ClassRoom(19);
+  const object2 = new ClassRoom(20);
+  const object3 = new ClassRoom(34);
 
   // collecting all objects into an array
-  classRoomObjects.push(...[object1, object2, object3])
+  classRoomObjects.push(...[object1, object2, object3]);
 
-  return classRoomObjects
-}
+  return classRoomObjects;
+};
+
+
