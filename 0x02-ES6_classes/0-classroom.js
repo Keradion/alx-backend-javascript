7fa0a15274077c3f151d72d0452a94cdcd69cd39
Daniel <danielshitaye10@gmail.com>
2025-03-17 13:04:13 +0300

    Define a class called ClassRoom and provide a constructor that assign an argument passsed to it into the object property using this

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize
+  }
+}
+
+// we define a Class Called 'ClassRoom' .
+// class keyword has been used to define a class.
