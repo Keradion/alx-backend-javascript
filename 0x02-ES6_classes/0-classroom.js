0060cb043ae9a6080da3b59fa6f940d9bb9cc818
Daniel <danielshitaye10@gmail.com>
2025-03-17 13:31:37 +0300

    lint issues fixed

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,8 +1,5 @@
 export default class ClassRoom {
   constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
-
-// we define a Class Called 'ClassRoom' .
-// class keyword has been used to define a class.
