83d58d37f8d2a571eb2ee02780ff8337a1f9bda5
Daniel <danielshitaye10@gmail.com>
2025-03-17 13:13:35 +0300

    Create 3 classRoom objects collect all into an array and return

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,17 @@
+// we are importing class ClassRoom 
+import ClassRoom from './0-classroom.js'
+
+export default function initalizeRooms (){
+  // Initalize an empty array to collect ClassRoom Objects here
+  const classRoomObjects = []
+
+  // Create 3 ClassRoom Objects 
+  const object1 = new ClassRoom(19)
+  const object2 = new ClassRoom(20)
+  const object3 = new ClassRoom(34)
+
+  // collecting all objects into an array
+  classRoomObjects.push(...[object1, object2, object3])
+
+  return classRoomObjects
+}
