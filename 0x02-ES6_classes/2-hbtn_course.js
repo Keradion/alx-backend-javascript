368f00091e048531540e67cf8ee82622d19d10e8
Daniel <danielshitaye10@gmail.com>
2025-03-17 14:10:26 +0300

    solve lint issue

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,15 +1,15 @@
 export default class ALXCourse {
-  constructor(name, length, students){
+  constructor(name, length, students) {
     if (typeof name !== 'string') {
-      throw Error('TypeError: Name must be a string')
+      throw Error('TypeError: Name must be a string');
     }
     
     if (typeof length !== 'number'){
-      throw Error('TypeError: Length must be a number')
+      throw Error('TypeError: Length must be a number');
     }
 
     if (!(Array.isArray(students))){
-      throw Error('TypeError: Students must be array of strings')
+      throw Error('TypeError: Students must be array of strings');
     }
 
     this._name = name;
@@ -18,26 +18,35 @@ export default class ALXCourse {
   }
 
   get name() {
-    return this._name
+    return this._name;
   }
 
   set name(name) {
-    this._name = name
+    if (typeof name !== 'string'){
+      throw Error('TypeError: Name must be a string');
+    }
+    this._name = name;
   }
 
   get length() {
-    return this._length
+    return this._length;
   }
 
   set length(length) {
-    this._length = length
+    if (typeof length !== 'number'){
+      throw Error('TypeError: Length must be a Number');
+    }
+    this._length = length;
   }
 
   get students() {
-    return this._students
+    return this._students;
   }
 
   set students (students) {
-    this._students = students
+    if (!(Array.isArray(students))){
+      throw Error('TypeError: Students must be an array of strings')
+    }
+    this._students = students;
   }
 }
