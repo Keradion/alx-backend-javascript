a8b0194eac18d3c9478256bc171b7e1620507144
Daniel <danielshitaye10@gmail.com>
2025-03-19 15:11:34 +0300

    valueOf() and toString() to get numeric and string representation of an object

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,14 @@
+export default class ALXClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+  
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
