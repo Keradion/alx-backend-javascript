fc1d72170069d93d276e610a5eafc4a2ad51a633
Daniel <danielshitaye10@gmail.com>
2025-03-19 15:39:40 +0300

    hoisting issue resolved, we moved the obejcts inistantiation after the class's definition'

---
+++ 0x02-ES6_classes/9-hoisting.js
@@ -0,0 +1,47 @@
+export class ALXClass {
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
+
+  get year() {
+    return this._year;
+  }
+
+  get location() {
+    return this._location;
+  }
+}
+
+const class2019 = new ALXClass(2019, 'San Francisco');
+const class2020 = new ALXClass(2020, 'San Francisco');
+
+
+
+export class StudentALX {
+  constructor(firstName, lastName, alxClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._alxClass = alxClass;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get alxClass() {
+    return this.alxClass;
+  }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._alxClass.year} - ${this._alxClass.location}`;
+  }
+}
+
+const student1 = new StudentALX('Guillaume', 'Salva', class2020);
+const student2 = new StudentALX('John', 'Doe', class2020);
+const student3 = new StudentALX('Albert', 'Clinton', class2019);
+const student4 = new StudentALX('Donald', 'Bush', class2019);
+const student5 = new StudentALX('Jason', 'Sandler', class2019);
+
+export const listOfStudents = [student1, student2, student3, student4, student5];
