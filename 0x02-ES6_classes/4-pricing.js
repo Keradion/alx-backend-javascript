41c808e7751d9eb0437cf0678d482c0f0b9a44c8
Daniel <danielshitaye10@gmail.com>
2025-03-19 13:38:56 +0300

    output correction

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -31,7 +31,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency._name} ${(this._currency._code)}`
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`
   }
   static convertPrice(amount, conversionRate) {
     return (amount * conversionRate)
