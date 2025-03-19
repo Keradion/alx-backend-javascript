3f821ff7de2067a8058041bb5271d7870a458edc
Daniel <danielshitaye10@gmail.com>
2025-03-19 13:59:48 +0300

    we define an asbtract class that act as a base class for other classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,11 +1,16 @@
 export default class Building {
   constructor(sqft) {
     if (typeof sqft !== 'number') {
-      throw Error('TypeError: sqft must be a Number')
+      throw Error('TypeError: Sqft must be a Number')
     }
-    this._sqft = sqft
+    this._sqft = sqft;
+
+    if (this.evacuationWarningMessage == undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage')
+    }
   }
-  get sqft(){
-    return this._sqft
+  
+  get sqft() {
+    return this._sqft;
   }
 }
