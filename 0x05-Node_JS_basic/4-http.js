1f3cf9d19e27219c6a70092738988766d71ffffd
Daniel <danielshitaye10@gmail.com>
2025-03-28 19:13:56 +0300

    a basic http server that listen on a specific port and respond for requests

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const app = require('http');
+
+// Create a server first
+const requestListener = (req, res) => {
+  res.end('Hello ALX!')
+}
+
+const Server = app.createServer(requestListener);
+
+Server.listen(1245, 'localhost', () => {
+  console.log('Server is Active.')
+});
+
+module.exports = app;
