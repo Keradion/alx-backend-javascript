516d4bc9cfff6b3a4f350bb7f11cb5fd9a5db09f
Daniel <danielshitaye10@gmail.com>
2025-03-28 15:16:47 +0300

    read a line from command line and terminate the running program when a user enter ctrl-l

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -10,5 +10,5 @@ rline.question('Welcome to Holberton School, what is your name?\n', (name) => {
   // Message displays when the user try to close or end the program - close event.
   rline.on('close', () => {
     console.log('This important software is now closing');
-  })
+  });
 });
