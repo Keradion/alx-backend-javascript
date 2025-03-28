48ec45132acf5d30b64da34c7ef2d42ac1c7ef5c
Daniel <danielshitaye10@gmail.com>
2025-03-28 20:12:53 +0300

    handle more complex http server with node

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,23 +1,8 @@
 const fs = require('fs').promises;
+
 const http = require('http');
 const filePath = process.argv[2];
 
-const requestListener = (req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200);
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    countStudents(filePath).then((response) => {
-      res.end(response);
-      res.writeHead(200);
-  })
-};
-}
-
-const app = http.createServer(requestListener);
-
-app.listen(1245, () => {
-});
 
 const processFile = (file) => {
   const cs = [];
@@ -40,11 +25,33 @@ const countStudents = async (filePath) => {
   try {
     const file = await fs.readFile(filePath, 'utf-8');
     const students = processFile(file.split('\n'));
-    return `This is the list of our students\nNumber of students: ${students[0].length + students[1].length}\nNumber of students in CS: ${students[0].length}. List: ${students[0].join(', ')}\nNumber of students in SWE: ${students[1].length}. List: ${students[1].join(', ')}
-    `
+    return `This is the list of our students\nNumber of students: ${students[0].length + students[1].length}\nNumber of students in CS: ${students[0].length}. List: ${students[0].join(', ')}\n
+    `;
   } catch (error) {
     throw Error('Cannot load the database');
   }
+};
+
+const requestListener = (req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200);
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(filePath).then((response) => {
+      res.end(response);
+      res.writeHead(200);
+  }).catch ((error) => {
+    const response = `This is the list of our students\n` + error
+    res.writeHead(200);
+    res.end(response);
+  })
+};
 }
+
+const app = http.createServer(requestListener);
+
+app.listen(1245, () => {
+});
+
 
 module.exports = app;
