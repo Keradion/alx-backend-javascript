06fec95cf1637e624b6182073a5ec95faaf35b34
Daniel <danielshitaye10@gmail.com>
2025-03-28 15:25:49 +0300

    read a line from command line and terminate the running program when a user enter ctrl-l

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 const readline = require('readline');
 
 const rline = readline.createInterface({
@@ -7,8 +8,10 @@ const rline = readline.createInterface({
 
 rline.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
-  // Message displays when the user try to close or end the program - close event.
-  rline.on('close', () => {
+});
+
+// Message displays when the user try to close or end the program - close event.
+process.on('SIGINT', () => {
     console.log('This important software is now closing');
-  });
+    rline.close();
 });
