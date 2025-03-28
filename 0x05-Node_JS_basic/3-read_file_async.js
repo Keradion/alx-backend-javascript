fbc20363869738a04a2c3e8d3c19e789c53f1afa
Daniel <danielshitaye10@gmail.com>
2025-03-28 18:42:32 +0300

    print students in a format and return a promise - reading a file asynchronusely

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,30 @@
+const fs = require('node:fs/promises');
+const processFile = (file) => {
+  const cs = [];
+  const swe = [];
+  for (const line of file.slice(1)) {
+    const row = line.split(',')
+    const name = row[0];
+    const filed = row[row.length - 1].trim();
+
+    if (filed === 'CS') {
+      cs.push(name);
+    } else if (filed === 'SWE') {
+      swe.push(name);
+    }
+  }
+  return [cs, swe];
+};
+
+const countStudents = async (filePath) => {
+   try{
+    const file = await fs.readFile(filePath, 'utf-8');
+    const students = processFile(file.split('\n'));
+    console.log(`Number of students: ${students[0].length + students[1].length}`)
+    console.log(`Number of students in CS: ${students[0].length}. List: ${students[0].join(', ')}`);
+    console.log(`Number of students in SWE: ${students[1].length}. List: ${students[1].join(', ')}`);
+  } catch {
+    throw new Error('Cannot load the database')
+  }
+};
+module.exports = countStudents;
