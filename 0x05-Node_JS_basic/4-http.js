1b11cc9b25321895346828cf41aeff3e7f5c2e84
Daniel <danielshitaye10@gmail.com>
2025-03-28 19:15:35 +0300

    put correct status code for the route

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,13 +2,14 @@ const app = require('http');
 
 // Create a server first
 const requestListener = (req, res) => {
-  res.end('Hello ALX!')
+  res.writeHead(200);
+  res.end('Hello ALX!');
 }
 
 const Server = app.createServer(requestListener);
 
 Server.listen(1245, 'localhost', () => {
-  console.log('Server is Active.')
+  console.log('Server is Active.');
 });
 
 module.exports = app;
