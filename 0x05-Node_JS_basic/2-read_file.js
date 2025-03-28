510ab8620eb047831a2058ea35d39cdbd2de1736
Daniel <danielshitaye10@gmail.com>
2025-03-28 15:29:14 +0300

    read a file synchronusly - csv file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,24 +1,11 @@
 const fs = require('fs');
 
-const countStudents = (filePath) => {
-  try
-  {
-    const file = fs.readFileSync(filePath, 'utf-8'); 
-    processFile((file).split('\n')); 
-  } 
-  catch (Error)
-  {
-    throw new Error('Cannot load the database')
-  }
-}
-
-
 const processFile = (file) => {
   const swe = [];
   const cs = [];
   for (const line of file.slice(1)) {
     // split the line using ',' and fetch the first name of the student 
-    const row = line.split(',')
+    const row = line.split(',');
     const name = row[0];
     const department = row[row.length - 1].trim();
     if (department === 'CS') {
@@ -36,6 +23,17 @@ const displayData = (cs, swe) => {
   console.log(`Number of students: ${totalNumberOfStudents}`);
   console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
   console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+}
+
+
+const countStudents = (filePath) => {
+  try{
+    const file = fs.readFileSync(filePath, 'utf-8'); 
+    processFile((file).split('\n')); 
+  } catch (Error)
+  {
+    throw new Error('Cannot load the database');
+  }
 }
 
 // The function accept a file path as an argument and read the file synchrounesly.
