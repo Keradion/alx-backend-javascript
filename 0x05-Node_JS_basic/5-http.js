108ebca86445024a604523049ec9d849cb5d6cb0
Daniel <danielshitaye10@gmail.com>
2025-03-28 20:03:06 +0300

    handle more complex http server with node

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,50 @@
+const fs = require('fs').promises;
+const http = require('http');
+const filePath = process.argv[2];
+
+const requestListener = (req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200);
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    countStudents(filePath).then((response) => {
+      res.end(response);
+      res.writeHead(200);
+  })
+};
+}
+
+const app = http.createServer(requestListener);
+
+app.listen(1245, () => {
+});
+
+const processFile = (file) => {
+  const cs = [];
+  const swe = [];
+  for (const line of file.slice(1)) {
+    const row = line.split(',');
+    const name = row[0];
+    const filed = row[row.length - 1].trim();
+
+    if (filed === 'CS') {
+      cs.push(name);
+    } else if (filed === 'SWE') {
+      swe.push(name);
+    }
+  }
+  return [cs, swe];
+};
+
+const countStudents = async (filePath) => {
+  try {
+    const file = await fs.readFile(filePath, 'utf-8');
+    const students = processFile(file.split('\n'));
+    return `This is the list of our students\nNumber of students: ${students[0].length + students[1].length}\nNumber of students in CS: ${students[0].length}. List: ${students[0].join(', ')}\nNumber of students in SWE: ${students[1].length}. List: ${students[1].join(', ')}
+    `
+  } catch (error) {
+    throw Error('Cannot load the database');
+  }
+}
+
+module.exports = app;
