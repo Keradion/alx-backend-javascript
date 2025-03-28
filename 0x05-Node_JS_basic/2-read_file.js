cb09393b858c974212c168d0bce241967feae9f2
Daniel <danielshitaye10@gmail.com>
2025-03-28 15:33:41 +0300

    read a file synchronusly - csv file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,5 +1,12 @@
 const fs = require('fs');
 
+const displayData = (cs, swe) => {
+  const totalNumberOfStudents = cs.length + swe.length;
+  console.log(`Number of students: ${totalNumberOfStudents}`);
+  console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+  console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+};
+
 const processFile = (file) => {
   const swe = [];
   const cs = [];
@@ -9,32 +16,22 @@ const processFile = (file) => {
     const name = row[0];
     const department = row[row.length - 1].trim();
     if (department === 'CS') {
-      cs.push(name)
-    }
-    else if (department === 'SWE') {
-      swe.push(name) 
+      cs.push(name);
+    } else if (department === 'SWE') {
+      swe.push(name);
     }
   }
   displayData(cs, swe);
-}
-
-const displayData = (cs, swe) => {
-  const totalNumberOfStudents = cs.length + swe.length;
-  console.log(`Number of students: ${totalNumberOfStudents}`);
-  console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
-  console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
-}
-
+};
 
 const countStudents = (filePath) => {
-  try{
-    const file = fs.readFileSync(filePath, 'utf-8'); 
-    processFile((file).split('\n')); 
-  } catch (Error)
-  {
+  try {
+    const file = fs.readFileSync(filePath, 'utf-8');
+    processFile((file).split('\n'));
+  } catch (Error) {
     throw new Error('Cannot load the database');
   }
-}
+};
 
 // The function accept a file path as an argument and read the file synchrounesly.
 
