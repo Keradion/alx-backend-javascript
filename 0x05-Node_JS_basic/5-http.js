ae0ea3f038d0e7a00491c502161c086f0e723630
Daniel <danielshitaye10@gmail.com>
2025-03-30 14:55:32 +0300

    nodejs server with 2 routes

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,8 +1,8 @@
 const fs = require('fs').promises;
 
 const http = require('http');
-const filePath = process.argv[2];
 
+const filePath = process.argv[2];
 
 const processFile = (file) => {
   const cs = [];
@@ -25,8 +25,7 @@ const countStudents = async (filePath) => {
   try {
     const file = await fs.readFile(filePath, 'utf-8');
     const students = processFile(file.split('\n'));
-    return `This is the list of our students\nNumber of students: ${students[0].length + students[1].length}\nNumber of students in CS: ${students[0].length}. List: ${students[0].join(', ')}\n
-    `;
+    return `This is the list of our students\nNumber of students: ${students[0].length + students[1].length}\nNumber of students in CS: ${students[0].length}. List: ${students[0].join(', ')}\nNumber of students in SWE: ${students[1].length}. List: ${students[1].join(', ')}`;
   } catch (error) {
     throw Error('Cannot load the database');
   }
@@ -38,20 +37,19 @@ const requestListener = (req, res) => {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     countStudents(filePath).then((response) => {
-      res.end(response);
       res.writeHead(200);
-  }).catch ((error) => {
-    const response = `This is the list of our students\n` + error
+      res.end(response);
+    }).catch ((error) => {
+    const response = `This is the list of our students\n${error}`;
     res.writeHead(200);
     res.end(response);
-  })
+    })
+  }
 };
-}
 
 const app = http.createServer(requestListener);
 
 app.listen(1245, () => {
 });
-
 
 module.exports = app;
