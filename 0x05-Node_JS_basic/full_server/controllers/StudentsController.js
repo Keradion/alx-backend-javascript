d2badfc94f4a6b1f9f61de54d34b7a6efdb9304b
Daniel <danielshitaye10@gmail.com>
2025-04-02 18:53:51 +0300

    import issue resolved in fs promises case

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,45 +1,44 @@
 import readDatabase from '../utils.js'
+
 const filePath = process.argv[2];
 
 class StudentsController {
-  static async getAllStudents (request, response) {
+  static async getAllStudents(request, response) {
     // Calling an asycnhronous function to fetch student data.
     try {
       const data = await readDatabase(filePath); // calling readDatabse
       const fileds = Object.keys(data);
       let result = '';
       for (const filed of fileds) {
-        result = result + `Number of students in ${filed}:  ${data[filed].length}. List: ${data[filed].join(',  ')}<br>`;
+        result = `Number of students in ${filed}:  ${data[filed].length}. List: ${data[filed].join(',  ')}<br>`;
       }
       response.status(200);
       response.send(result);
-    } catch (error){
+    } catch (error) {
       response.status(500);
-      response.send('Cannot load the database')
+      response.send('Cannot load the database');
     }
   }
 
-  static async getAllStudentsByMajor (request, response) {
-    const major = request.params.major.toUpperCase(); // get the requested major name
+  static async getAllStudentsByMajor(request, response) {
+    const major = request.params.major.toUpperCase(); 
     let result = '';
-    
     try {
-      const data = await readDatabase(filePath); // calling readDatabse
-    if (major === ':CS') {
-      result = `Number of students in CS:  ${data['CS'].length}. List: ${data['CS'].join(',  ')}`;
+      const data = await readDatabase(filePath); 
+      if (major === ':CS') {
+        result = `Number of students in CS:  ${data.CS.length}. List: ${data.CS.join(',  ')}`;
 
-    } else if (major === ':SWE') {
-      result = `Number of students in SWE:  ${data['SWE'].length}. List: ${data['SWE'].join(',')}`;
-    } else {
-      response.status(500);
-      response.send('Major Parameter must be CS or SWE')
+      } else if (major === ':SWE') {
+        result = `Number of students in SWE:  ${data.SWE.length}. List: ${data.SWE.join(',')}`;
+      } else {
+        response.status(500);
+        response.send('Major Parameter must be CS or SWE')
     }
-
-    response.status(200);
-    response.send(result);
+       response.status(200);
+       response.send(result);
   } catch (error) {
-    response.status(500);
-    response.send(error);
+       response.status(500);
+       response.send(error);
   }
   }
 }
