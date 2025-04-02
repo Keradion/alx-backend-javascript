be120b7f4558e37e556c23cfd5f3980b1a94da9a
Daniel <danielshitaye10@gmail.com>
2025-04-02 18:40:20 +0300

    import issue resolved in fs promises case

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -4,7 +4,7 @@ import StudentsController from '../controllers/StudentsController.js';
 
 const router = express.Router();
 
-router.get('/', AppController.getHomePage);
+router.get('/', AppController.getHomepage);
 router.get('/students', StudentsController.getAllStudents);
 router.get('/students/:major', StudentsController.getAllStudentsByMajor);
 
