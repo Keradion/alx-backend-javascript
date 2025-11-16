147cd3a83faabcdd55b78ea85beb42258a45b65c
DANiel Berihun <danielshitaye10@gmail.com>
2025-11-16 20:04:51 +0000

    Test calculateNumber done

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,22 +1,22 @@
-const assert = require('node:assert'); // Importing nodes assert library for testing
+const assert = require('assert'); // Importing nodes assert library for testing
 const calculateNumber = require('./0-calcul'); // Importing the function to be tested
 
 
+describe('CalculateNumber Test Cases', () => {
+	
+	it('Sum of 1.5 and 0.5 is 3', () => {
+		assert.equal(calculateNumber(1.5, 0.5), 3);
+	});
 
-describe("calculateNumber", () => {
-  
-  it('The sum of 2.5 and 3 is 6', () => {
-    assert.strictEqual(calculateNumber(2.5, 3), 6);
-  });
+	it('Sum of 1 and 2.5 is 4', () => {
+		assert.equal(calculateNumber(1, 2.5), 4);
+	});
 
-  it('The sum of 1.75 and 8.56 is 11', () => {
-    assert.strictEqual(calculateNumber(1.75, 8.56), 11);
-  });
+	it('Sum of 7.7 and 2.3 is 10', () => {
+		assert.equal(calculateNumber(7.7, 2.3), 10);
+	});
 
-  it('The sum of 3.45 and 1 is 4 ', () => {
-    assert.strictEqual(calculateNumber(3.45, 1), 4);
-  });
-
+	it('Sum of -5.3 and 2.5 is -2', () => {
+		assert.strictEqual(calculateNumber(-5.3, 2.5), -2);
+	});
 });
-
-
