cb4a2e0885155d6ac55e318030ffbbe7390fb500
DANiel Berihun <danielshitaye10@gmail.com>
2025-11-17 15:32:32 +0000

    update the arate the argument position of type into 1st parameter

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,19 +1,16 @@
-const calculateNumber = (a, b, type) => {
+const calculateNumber = (type, a, b) => {
+  const roundA = Math.round(a);
+  const roundB = Math.round(b);
 
-	const roundA = Math.round(a);
-	const roundB = Math.round(b);
+  if (type === 'SUM') return roundA + roundB;
 
-	if (type === 'SUM') return (roundA + roundB);
+  if (type === 'SUBTRACT') return roundA - roundB;
 
-	if (type === 'SUBTRACT') return (roundA - roundB);
-	
-	if (type === 'DIVIDE') {
-		if (roundB === 0) {
-			return 'Error';
-		}
-
-		return (roundA / roundB);
-	}
-}
+  if (type === 'DIVIDE') {
+    if (roundB === 0) return 'Error';
+    return roundA / roundB;
+  }
+};
 
 module.exports = calculateNumber;
+
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,55 +1,51 @@
 const { expect } = require('chai');
-const calculateNumber = require('./1-calcul.js');
-
+const calculateNumber = require('./2-calcul_chai.js');
 
 describe('Test Type SUM', () => {
-	
-	it('sum of 1 and 2 is 3', () => {
-		expect(calculateNumber(1, 2, 'SUM')).to.equal(3);
-	});
 
+  it('sum of 1 and 2 is 3', () => {
+    expect(calculateNumber('SUM', 1, 2)).to.equal(3);
+  });
 
-        it('sum of -1 and -2 is -3', () => {
-		expect(calculateNumber(-1, -2, 'SUM')).to.equal(-3);
-        });
+  it('sum of -1 and -2 is -3', () => {
+    expect(calculateNumber('SUM', -1, -2)).to.equal(-3);
+  });
 
-
-        it('sum of 5.1 and 2.8 is 8', () => {
-                expect(calculateNumber(5.1, 2.8, 'SUM')).to.equal(8);
-        });
+  it('sum of 5.1 and 2.8 is 8', () => {
+    expect(calculateNumber('SUM', 5.1, 2.8)).to.equal(8);
+  });
 
 });
 
 describe('Test Type SUBTRACT', () => {
 
+  it('Subtraction of 10 and 2 is 8', () => {
+    expect(calculateNumber('SUBTRACT', 10, 2)).to.equal(8);
+  });
 
-        it('Subtraction of 10 and 2 is 8', () => {
-                expect(calculateNumber(10, 2, 'SUBTRACT')).to.equal(8);
-        });
+  it('Subtraction of 1 and -2 is 3', () => {
+    expect(calculateNumber('SUBTRACT', 1, -2)).to.equal(3);
+  });
 
+  it('Subtraction of 5.5 and 2.8 is 3', () => {
+    expect(calculateNumber('SUBTRACT', 5.5, 2.8)).to.equal(3);
+  });
 
-        it('Subtraction of 1 and -2 is 3', () => {
-                expect(calculateNumber(1, -2, 'SUBTRACT')).to.equal(3);
-        });
-
-
-        it('Subtraction of 5.5 and 2.8 is 3', () => {
-                expect(calculateNumber(5.5, 2.8, 'SUBTRACT')).to.equal(3);
-        });
 });
 
 describe('Test Type DIVIDE', () => {
 
+  it('Division of 1 and 2 is 0.5', () => {
+    expect(calculateNumber('DIVIDE', 1, 2)).to.equal(0.5);
+  });
 
-        it('Division of 1 and 2 is 0.5', () => {
-                expect(calculateNumber(1, 2, 'DIVIDE')).to.equal(0.5);
-        });
+  it('Division of 4 and 2 is 2', () => {
+    expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
+  });
 
-	it('Division of 4 and 2 is 2', () => {
-                expect(calculateNumber(4, 2, 'DIVIDE')).to.equal(2);
-        });
+  it('Division of 1 and 0 is Error', () => {
+    expect(calculateNumber('DIVIDE', 1, 0)).to.equal('Error');
+  });
 
-	it('Division of 1 and 0 is Error', () => {
-                expect(calculateNumber(1, 0, 'DIVIDE')).to.equal('Error');
-        });
 });
+
